3eea147b7cc702669bbd9a02df6b4db78678c337
dev-carlosgomez <111075484+dev-carlosgomez@users.noreply.github.com>
2022-08-12 16:50:19 -0500

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -30,58 +30,23 @@ weatherForm.addEventListener('submit', (e) => {
                 messageOne.textContent = data.error
             } else {
                 messageOne.textContent = data.error
-                messageTwo.textContent = data.location.name               
+                messageTwo.textContent = 'Resultados: '   
+                clima1.textContent = '<h2>Ciudad:</h2> ' + data.location.name
+                clima2.textContent = '<h2>País: </h2>' + data.location.country
+                clima3.textContent = '<h2>Hora actual: </h2>' + data.current.observation_time
+                clima4.textContent = '<h2>Temperatura: </h2>' + data.current.temperature
+                clima5.textContent = '<h2>Codigo: </h2>' + data.current.weather_code
+                clima6.textContent = '<h2>Clima: </h2>' + data.current.weather_descriptions
+                clima7.textContent = '<h2>Velocidad del viento: </h2>' + data.current.wind_speed
+                clima8.textContent = '<h2>Dirección del viento: </h2>' + data.current.wind_dir
+                clima9.textContent = '<h2>Humedad: </h2>' + data.current.humidity
+                clima10.textContent = '<h2>Porcentaje de nubes: </h2>' + data.current.cloudcover            
             }
         })
     })
 
 
-    var requestOptions = {
-        method: 'POST',
-        redirect: 'follow'
-      };
-      
-      fetch('http://api.weatherstack.com/current?access_key=efb94ed60531139ab798b85c3653fbd0&query=' + location, requestOptions)
-      .then(response => response.text())
-      .then(result => 
-        contenidojson = result
-        )
-      .catch(error => console.log('error', error));
-      messageOne.textContent = ''
-      messageTwo.textContent = 'Resultado: '
-      
-
-          console.log("Respuesta: ")
-        console.log(contenidojson)
-
-        objlocation = JSON.parse(contenidojson)
-        
-        clima1.textContent = '<h2>Ciudad:</h2> ' + objlocation.location.name
-        clima2.textContent = '<h2>País: </h2>' + objlocation.location.country
-        clima3.textContent = '<h2>Hora actual: </h2>' + objlocation.current.observation_time
-        clima4.textContent = '<h2>Temperatura: </h2>' + objlocation.current.temperature
-        clima5.textContent = '<h2>Codigo: </h2>' + objlocation.current.weather_code
-//        clima6.textContent = objlocation.current.weather_icons
-        clima6.textContent = '<h2>Clima: </h2>' + objlocation.current.weather_descriptions
-        clima7.textContent = '<h2>Velocidad del viento: </h2>' + objlocation.current.wind_speed
-        clima8.textContent = '<h2>Dirección del viento: </h2>' + objlocation.current.wind_dir
-        clima9.textContent = '<h2>Humedad: </h2>' + objlocation.current.humidity
-        clima10.textContent = '<h2>Porcentaje de nubes: </h2>' + objlocation.current.cloudcover
-
-
-        console.log(objlocation.location.name)
-        console.log(objlocation.location.country)
-        console.log(objlocation.current.observation_time)
-        console.log(objlocation.current.temperature)
-        console.log(objlocation.current.weather_code)
-        console.log(objlocation.current.weather_icons)
-        console.log(objlocation.current.weather_descriptions)
-        console.log(objlocation.current.wind_speed)
-        console.log(objlocation.current.wind_dir)
-        console.log(objlocation.current.humidity)
-        console.log(objlocation.current.cloudcover)
-
-
+    
         
 
 })
