0e0aa002e3252bbddf05ebd6a41485ddbecb327e
dev-carlosgomez <111075484+dev-carlosgomez@users.noreply.github.com>
2022-08-12 12:25:32 -0500

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -33,7 +33,7 @@ weatherForm.addEventListener('submit', (e) => {
       fetch('http://api.weatherstack.com/current?access_key=efb94ed60531139ab798b85c3653fbd0&query=' + location, requestOptions)
       .then(response => response.text())
       .then(result => 
-        messageOne.textContent = result
+        messagetres.textContent = result
         )
       .catch(error => console.log('error', error));
 
