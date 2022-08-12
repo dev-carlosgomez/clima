8b9f92a65cf9425a00bb39a8488cea1ccf735c0a
dev-carlosgomez <111075484+dev-carlosgomez@users.noreply.github.com>
2022-08-12 11:21:22 -0500

    https

--- public/js/app.js
+++ public/js/app.js
@@ -34,7 +34,7 @@ var requestOptions = {
     redirect: 'follow'
   };
   
-  fetch('http://api.weatherstack.com/current?access_key=efb94ed60531139ab798b85c3653fbd0&query=' + location, requestOptions)
+  fetch('https://api.weatherstack.com/current?access_key=efb94ed60531139ab798b85c3653fbd0&query=' + location, requestOptions)
     .then(response => response.text())
     .then(result => console.log(result))
     .catch(error => console.log('error', error));
