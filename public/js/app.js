bcafb8aacef4ccad76727a42dc6c067d6ddb1227
dev-carlosgomez <111075484+dev-carlosgomez@users.noreply.github.com>
2022-08-12 16:10:31 -0500

    Cargando resultados

--- public/js/app.js
+++ public/js/app.js
@@ -2,8 +2,17 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-const messagetres = document.querySelector('#message-3')
-const messagecuatro = document.querySelector('#message-4')
+const clima1 = document.querySelector('#clima1')
+const clima2 = document.querySelector('#clima2')
+const clima3 = document.querySelector('#clima3')
+const clima4 = document.querySelector('#clima4')
+const clima5 = document.querySelector('#clima5')
+const clima6 = document.querySelector('#clima6')
+const clima7 = document.querySelector('#clima7')
+const clima8 = document.querySelector('#clima8')
+const clima9 = document.querySelector('#clima9')
+const clima10 = document.querySelector('#clima10')
+
 var contenidojson = 0
 
 weatherForm.addEventListener('submit', (e) => {
@@ -26,7 +35,7 @@ weatherForm.addEventListener('submit', (e) => {
         contenidojson = result
         )
       .catch(error => console.log('error', error));
-      messageOne.textContent = '...'
+      messageOne.textContent = ''
       messageTwo.textContent = 'Resultado: '
       
 
@@ -34,9 +43,20 @@ weatherForm.addEventListener('submit', (e) => {
         console.log(contenidojson)
 
         objlocation = JSON.parse(contenidojson)
-        messagetres.textContent = objlocation
-        messagecuatro.textContent = '...'
         
+        clima1.textContent = objlocation.location.name
+        clima2.textContent = objlocation.location.country)
+        clima3.textContent = objlocation.current.observation_time)
+        clima4.textContent = objlocation.current.temperature)
+        clima5.textContent = objlocation.current.weather_code)
+//        clima6.textContent = objlocation.current.weather_icons)
+        clima6.textContent = objlocation.current.weather_descriptions)
+        clima7.textContent = objlocation.current.wind_speed)
+        clima8.textContent = objlocation.current.wind_dir)
+        clima9.textContent = objlocation.current.humidity)
+        clima10.textContent = objlocation.current.cloudcover)
+
+
         console.log(objlocation.location.name)
         console.log(objlocation.location.country)
         console.log(objlocation.current.observation_time)
